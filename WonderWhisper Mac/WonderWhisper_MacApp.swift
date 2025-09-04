15dc21b3050b3ca1eb90b2017fd87a0caec78cac
slumdev88 <dane.kapoor@gmail.com>
2025-09-04 13:02:27 +0800

    Initial Commit

---
+++ WonderWhisper Mac/WonderWhisper_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  WonderWhisper_MacApp.swift
+//  WonderWhisper Mac
+//
+//  Created by Dane Kapoor on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WonderWhisper_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WonderWhisper MacTests/WonderWhisper_MacTests.swift
@@ -0,0 +1,17 @@
+//
+//  WonderWhisper_MacTests.swift
+//  WonderWhisper MacTests
+//
+//  Created by Dane Kapoor on 4/9/25.
+//
+
+import Testing
+@testable import WonderWhisper_Mac
+
+struct WonderWhisper_MacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
